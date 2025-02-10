a9a0590c7ef93d8d85fc415a6605b5d4d96a47dc
Natalia Dinareli de Lima <natalia@sounuv.com>
2025-02-10 19:12:25 -0300

    Refinamento da estilização e responsividade da tela Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,41 +1,22 @@
-'use client';
+import React from "react";
 
-import { useState } from 'react';
 
 export default function Login() {
-  const [email, setEmail] = useState<string>('');
-  const [senha, setSenha] = useState<string>('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Email:', email);
-    console.log('Senha:', senha);
-  };
-
   return (
     <div className="container">
-      <div className="login-box">
-        <h2>Entre no Heart</h2>
-        <form className='login-form' onSubmit={handleSubmit}>
-          <div className="input-group">
+      <div className="content">
+        <div className="logo-section">
+          <img className="logo" src="./logo.png"/>
+          <h1 className="title">Entre no Heart</h1>
+        </div>
+        <form>
+          <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+            <input type="email" id="email" placeholder="Digite Seu Email"/>
           </div>
-          <div className="input-group">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-              required
-            />
+          <div className="form-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Digite Sua Senha"/>
           </div>
           <button type="submit">Entrar</button>
         </form>
